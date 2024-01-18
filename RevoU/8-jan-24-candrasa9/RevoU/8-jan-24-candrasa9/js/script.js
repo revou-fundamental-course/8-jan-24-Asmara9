6fd9c07b096b8cec33355c9bc0a8228623f57364
Candrasa Asmaradanta <155295765+Asmara9@users.noreply.github.com>
2024-01-18 23:09:22 +0700

    Update script.js

--- RevoU/8-jan-24-candrasa9/RevoU/8-jan-24-candrasa9/js/script.js
+++ RevoU/8-jan-24-candrasa9/RevoU/8-jan-24-candrasa9/js/script.js
@@ -8,27 +8,28 @@ function hitungLuas() {
   
     var luas = sideLength * sideLength;
     document.getElementById("luas").innerText = luas;
-  }
 
-  function resetForm() {
-    // Menghapus nilai pada input dan hasil luas
-    document.getElementById("sideLength").value = "";
-    document.getElementById("sisi").innerText = "";
-    document.getElementById("luas").innerText = "";
+    // Update calculation steps
+  document.getElementById("sisi").innerText = sideLength + " x " + sideLength;
   }
   
-  // Menambahkan tombol reset pada fungsi hitungLuas
-  function hitungLuas() {
-    var sideLength = document.getElementById("sideLength").value;
-  
-    if (sideLength === "" || isNaN(sideLength)) {
+  function hitungKeliling() {
+    var sisi = parseFloat(document.getElementById('sideLength').value);
+    if (sisi === "" || isNaN(sisi)) {
       alert("Masukkan panjang sisi dengan angka.");
       return;
     }
-  
-    var luas = sideLength * sideLength;
-    document.getElementById("luas").innerText = luas;
+    var keliling = 4 * sisi;
 
-     // Update calculation steps
-  document.getElementById("sisi").innerText = sideLength + " x " + sideLength;
-  }
+    document.getElementById('keliling').textContent = keliling;
+    document.getElementById("Sisi").innerText = 4 + " x " + sisi;
+}
+
+
+  function resetForm() {
+    document.getElementById('sideLength').value = '';
+    document.getElementById('sisi').textContent = '';
+    document.getElementById('Sisi').textContent = '';
+    document.getElementById('luas').textContent = '';
+    document.getElementById('keliling').textContent = '';
+}
